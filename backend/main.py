7e42f3c84bb8422fa3e1547fa5853b6ededd1805
Isita Mazumder <isitamazumder@gmail.com>
2026-09-10 02:48:04 +0530

    Refactor CORS middleware to use origins variable

--- backend/main.py
+++ backend/main.py
@@ -39,10 +39,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=origins,
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
